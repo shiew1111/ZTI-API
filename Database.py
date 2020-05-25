28fd8c009e362b995d3bbf25e0b67bda73cbf2c7
shiew1111 <43846075+shiew1111@users.noreply.github.com>
2020-05-25 12:06:27 +0200

    V1

---
+++ Database.py
@@ -0,0 +1,97 @@
+import sqlite3
+from sqlite3 import Error
+
+import os.path
+
+
+class DataBase:
+
+    def __init__(self, db_file="FarmDatabase.sqlite"):
+
+        self.db_file = db_file
+        self.creating_new_db = False
+
+        # check if database exists, if not create new, empty with table
+        if not os.path.isfile('FarmDatabase.sqlite'):
+            self.creating_new_db = True
+            self.createConnection()
+
+    def update(self, sqlUpdate, args):
+
+        conn = self.createConnection()
+        if conn is not None:
+
+            self.execSql(conn, sqlUpdate, args)
+        else:
+            print("Error! cannot create the database connection.")
+
+    def sqlSelectJson(self, farmId):
+        sql_select = """SELECT farmId, gold, productionLimit, timeOfharvest, timeOfsowing, 
+                       costOfsowing, updateCost,growTime, isHarvested FROM Farms  where farmId= ?;"""
+        conn = self.createConnection()
+        if conn is not None:
+
+            Sql_result = self.execSql(conn, sql_select, farmId)
+
+            FarmDataJson = {
+                "farmId": Sql_result[0][0],
+                "gold": Sql_result[0][1],
+                "productionLimit": Sql_result[0][2],
+                "timeOfHarvest": Sql_result[0][3],
+                "timeOfSowing": Sql_result[0][4],
+                "costOfSowing": Sql_result[0][5],
+                "updateCost": Sql_result[0][6],
+                "growTime": Sql_result[0][7],
+                "isHarvested": Sql_result[0][8],
+            }
+            return FarmDataJson
+        else:
+            print("Error! cannot create the database connection.")
+
+    # method that create Database with table Cards or just create connection
+    def createConnection(self):
+        conn = None
+        try:
+            conn = sqlite3.connect(self.db_file)
+
+            print(sqlite3.version)
+
+            if self.creating_new_db:
+                sql_create_table = """
+                CREATE TABLE IF NOT EXISTS Farms (
+                farmId integer PRIMARY KEY,
+                gold integer,
+                productionLimit integer,
+                timeOfharvest time,
+                timeOfsowing time,
+                costOfsowing integer,
+                updateCost integer,
+                growTime integer,
+                isHarvested integer
+                );
+                """
+                self.creating_new_db = False
+
+                if conn is not None:
+
+                    self.execSql(conn, sql_create_table)
+                else:
+                    print("Error! cannot create the database connection.")
+        except Error as e:
+            print(e)
+
+        return conn
+
+    # that method "takes" sql code and execute it
+    @staticmethod
+    def execSql(conn, sqlcode, *args):
+        try:
+            c = conn.cursor()
+            c.execute(sqlcode, *args)
+            conn.commit()
+            rows = c.fetchall()
+            return rows
+
+        except Error as e:
+            print(e)
+
---
+++ app.py
@@ -0,0 +1,157 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_cors import CORS
+import json
+from datetime import datetime, timedelta
+
+from Database import DataBase
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+with open('example_data.txt') as json_file:
+    example_data = json.load(json_file)
+
+
+def strfdelta(tdelta):
+    if tdelta < timedelta(0):
+        result = 0
+    else:
+        result = (tdelta.days * 24 * 60 * 60) + tdelta.seconds
+
+    return result
+
+
+class Forum(Resource):
+
+    def get(self, example_data=example_data):
+        return json.dumps(example_data)
+
+    def post(self, example_data=example_data):
+        new_post = request.get_json()
+        new_post["id"] = str(len(example_data["post"]) + 1)
+        example_data['post'].append(new_post)
+
+        with open('example_data.txt', 'w') as outfile:
+            json.dump(example_data, outfile)
+
+        return {'you sent:': new_post}
+
+
+class FarmGame(Resource):
+    def get(self):
+        action = request.args.get('action', default="", type=str)
+        farmId = str(request.args.get('farmId', default=0, type=int))
+
+        if farmId is not 0:
+            if DataBase().sqlSelectJson(farmId=farmId)['farmId']:
+                gold = DataBase().sqlSelectJson(farmId=farmId)['gold']
+                now = datetime.now()
+
+                if action == "Sowing":
+                    costOfSowing = DataBase().sqlSelectJson(farmId=farmId)['costOfSowing']
+
+                    if gold <= costOfSowing:
+                        StrTimeOFHarvest = now + timedelta(
+                            seconds=DataBase().sqlSelectJson(farmId="1")['growTime'])
+                        StrTimeOFHarvest.strftime("%d/%m/%Y %H:%M:%S")
+
+                        DataBase().update(
+                            """UPDATE Farms SET gold=?,timeOfsowing=?, timeOfharvest=?,isHarvested=? WHERE farmId= """ + farmId + """;""",
+                            (gold - DataBase().sqlSelectJson(farmId=farmId)['costOfSowing'],
+                             now.strftime("%d/%m/%Y %H:%M:%S"), StrTimeOFHarvest.strftime("%d/%m/%Y %H:%M:%S"), 0)
+                        )
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+                        return json.dumps(dumpsJson)
+                    else:
+
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+
+                        return json.dumps(dumpsJson)
+
+
+                elif action == "harvest":
+
+                    if now > datetime.strptime(DataBase().sqlSelectJson(farmId="1")['timeOfSowing'],
+                                               "%d/%m/%Y %H:%M:%S") + timedelta(
+                        seconds=DataBase().sqlSelectJson(farmId="1")['growTime']):
+                        DataBase().update(
+                            """UPDATE Farms SET gold=?, isHarvested=? WHERE farmId= """ + farmId + """;""",
+                            (gold + DataBase().sqlSelectJson(farmId=farmId)['productionLimit'], 1
+                             ))
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+                        return json.dumps(dumpsJson)
+                    else:
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+                        return json.dumps(dumpsJson)
+
+                elif action == 'UpdateFarm':
+
+                    updateCost = DataBase().sqlSelectJson(farmId=farmId)['updateCost']
+                    if gold <= updateCost:
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+                        return json.dumps(dumpsJson)
+                    else:
+                        DataBase().update(
+                            """UPDATE Farms SET gold=?,productionLimit=?, costOfsowing=? ,updateCost=?, growTime=?, isHarvested=? WHERE farmId= """ + farmId + """;""",
+                            (gold - updateCost,
+                             DataBase().sqlSelectJson(farmId=farmId)['productionLimit'] + 20,
+                             DataBase().sqlSelectJson(farmId=farmId)['costOfSowing'] + 15,
+                             DataBase().sqlSelectJson(farmId=farmId)['updateCost'] + 2,
+                             DataBase().sqlSelectJson(farmId=farmId)['growTime'] + 10,
+                             0))
+                        dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                        RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                          "%d/%m/%Y %H:%M:%S") - now
+
+                        dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+
+                        return json.dumps(dumpsJson)
+                else:
+                    dumpsJson = DataBase().sqlSelectJson(farmId=farmId)
+
+                    RemainingTime = datetime.strptime(dumpsJson['timeOfHarvest'],
+                                                      "%d/%m/%Y %H:%M:%S") - now
+
+                    dumpsJson["RemainingTime"] = strfdelta(RemainingTime)
+
+                    return json.dumps(dumpsJson)
+            else:
+
+                return "InvalidFarmID"
+        else:
+            return " FarmID = 0"
+
+
+api.add_resource(Forum, '/Forum')
+api.add_resource(FarmGame, '/FarmGame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
